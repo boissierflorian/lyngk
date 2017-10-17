b05d82362e84d687f8bce41c509fcea66c5a3a11
Boissier Florian <boissierflorian@outlook.com>
2017-10-17 13:24:20 +0200

    Test Story 8

--- src/Intersection.js
+++ src/Intersection.js
@@ -4,8 +4,20 @@ Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
 Lyngk.Intersection = function (c) {
     var state = Lyngk.State.VACANT;
+    var piece;
+    var coordinates = c;
 
     this.getState = function() {
         return state;
+    };
+
+    this.poserPiece = function(p) {
+        piece = p;
+        state = Lyngk.State.ONE_PIECE;
+    };
+
+    this.getColor = function() {
+      if (piece !== null && piece !== undefined)
+          return piece.getColor();
     };
 };
