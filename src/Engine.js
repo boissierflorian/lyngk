b3c24800c0c94fb5aaf7f9a7829d33c522e0b2e7
Boissier Florian <boissierflorian@outlook.com>
2017-11-13 14:36:19 +0100

    Implémentation story 24

--- src/Engine.js
+++ src/Engine.js
@@ -2,9 +2,11 @@
 
 // enums definition
 Lyngk.Color = {BLACK: 0, IVORY: 1, BLUE: 2, RED: 3, GREEN: 4, WHITE: 5};
+Lyngk.Player = {PLAYER_ONE : 0, PLAYER_TWO : 1};
 
 Lyngk.Engine = function () {
     var intersections = [];
+    var currentPlayer = Lyngk.Player.PLAYER_ONE;
 
     var _getRandom = function(max) {
         return Math.floor(Math.random() * max);
@@ -138,6 +140,10 @@ Lyngk.Engine = function () {
 
     this.getIntersections = function() {
       return intersections;
+    };
+
+    this.getCurrentPlayer = function() {
+        return currentPlayer;
     };
 
     init();
